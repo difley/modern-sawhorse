d73d3f433add61566adaec3317c97f67ded61c3a
Seth Difley <difley@gmail.com>
2017-08-30 07:17:14 -0500

    JSON from file to print Maybe Value.

--- app/Main.hs
+++ app/Main.hs
@@ -4,9 +4,10 @@ module Main where
 
 import Data.Aeson
 import Data.Aeson.Types
-import GHC.Exts
-import qualified Data.HashMap.Strict as HM
-import qualified Data.Text as T
+import Control.Monad.Catch
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
 import qualified Data.Text.Lazy.Encoding as T
 import qualified Data.Text.Lazy.IO as T
 import qualified Data.Vector as V
@@ -16,53 +17,26 @@ main = do
   print (decode "[1,2,3]" :: Maybe [Integer])
   print (eitherDecode "[]" :: Either String Integer)
   print (eitherDecode "[1,2,[3,true]]" :: Either String (Int, Int, (Int, Bool)))
+  x <- readJSONFileStrict "test.json"
+  print (Just (x :: Value))
+  print (parseMaybe parseArray x)
 
-
-
-  s <- T.encodeUtf8 <$> T.getLine
-  print (decode s :: Maybe Value)
-  print (parseMaybe parseArray =<< decode s)
 
 parseArray :: Value -> Parser [(String, Bool)]
 parseArray = withArray "array of tuples" $ \arr ->
                mapM parseTuple (V.toList arr)
 
--- parseArray (Array arr) = mapM parseTuple (V.toList arr)
--- parseArray _           = fail "expected an array"
 
-
--- parseTuple (Object obj) = do
---   let mbFieldA = HM.lookup "a" obj
--- 
---   fieldA <- case mbFieldA of
---     Just x -> return x
---     Nothing -> fail "no field 'a'"
--- 
---   a <- case fieldA of
---     String x -> return (T.unpack x)
---     _        -> fail "expected a string"
--- 
---   let b = True
--- 
---   return (a, b)
-
--- parseTuple = withObject "tuple" $ \obj -> do
---  a <- case HM.lookup "a" obj of
---    Just x -> parseJSON x
---    Nothing -> fail "no field 'a'"
---
---  b <- case HM.lookup "b" obj of
---    Just x -> parseJSON x
---    Nothing -> fail "no field 'b'"
---
---  return (a, b)
-
--- parseTuple = withObject "tuple" $ \o -> do
---  a <- o .: "a"
---  b <- o .: "b"
---  return (a, b)
-
+parseTuple :: Value -> Parser (String, Bool)
 parseTuple = withObject "tuple" $ \o -> do
   a <- o .: "a"
   b <- o .: "b"
   return (a, b)
+
+-- readJSONFileStrict :: (MonadIO m, FromJSON a) => FilePath -> m a
+readJSONFileStrict :: FilePath -> IO Value
+readJSONFileStrict fp = liftIO $ do
+  bs <- B.readFile fp
+  case eitherDecodeStrict' bs of
+    Left e -> throwM $ userError e
+    Right x -> return x
